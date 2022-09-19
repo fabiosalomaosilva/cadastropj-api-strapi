bc9dd34853728e2b591b4deb74826b6698716e05
Fábio Salomão Silva <fabio@arquivarnet.com.br>
2022-09-19 07:29:28 -0500

    update

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', 'db.uiqqicsiykrpwlxzerwo.supabase.co'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'EFW5WaPX!Q!SeFq'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
